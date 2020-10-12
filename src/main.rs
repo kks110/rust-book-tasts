e36a63c366aeb935a144abe6cb3462ffa4a88759
Kelvin Samuel <kelvin@kks110.com>
2020-10-13 00:24:26 +0100

    refactor out temp converter:

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,8 @@ extern crate rand;
 use std::cmp::Ordering;
 use rand::Rng;
 
-
 mod helper;
+mod temp_converter;
 
 fn main() {
     loop {
@@ -20,7 +20,7 @@ fn main() {
             guessing_game();
             break
         } else if option == "2" {
-            temp_converter();
+            temp_converter::convert();
             break
         } else if option == "3" {
             fibonacci();
@@ -34,44 +34,6 @@ fn main() {
         }
     }
 }
-
-fn temp_converter() {
-    loop {
-        println!("Welcome, what would you like to do?:");
-        println!("1) Convert to Fahrenheit ");
-        println!("2) Convert to Celsius");
-
-
-        let option = helper::get_user_input();
-
-        if option != "1" && option != "2" {
-            println!("Not a valid option, please try again.");
-            continue
-        };
-
-        println!("What temp would you like to convert?: ");
-        let temperature: f32 = helper::get_user_input_float();
-
-        if option == "1" {
-            println!("Your temp in fahrenheit is {}", convert_to_fahrenheit(temperature));
-            break
-        } else if option == "2" {
-            println!("Your temp in celsius is {}", convert_to_celsius(temperature));
-            break
-        }
-    }
-}
-
-fn convert_to_fahrenheit(temp: f32) -> f32 {
-    let multiplicative: f32 = 9.0/5.0;
-    (temp * multiplicative) + 32.0
-}
-
-fn convert_to_celsius(temp: f32) -> f32 {
-    let multiplicative: f32 = 5.0/9.0;
-    (temp - 32.0) * (multiplicative)
-}
-
 
 fn guessing_game() {
     println!("Guess the number!");
---
+++ src/temp_converter.rs
@@ -0,0 +1,38 @@
+use crate::helper;
+
+pub fn convert() {
+    loop {
+        println!("Welcome, what would you like to do?:");
+        println!("1) Convert to Fahrenheit ");
+        println!("2) Convert to Celsius");
+
+
+        let option = helper::get_user_input();
+
+        if option != "1" && option != "2" {
+            println!("Not a valid option, please try again.");
+            continue
+        };
+
+        println!("What temp would you like to convert?: ");
+        let temperature: f32 = helper::get_user_input_float();
+
+        if option == "1" {
+            println!("Your temp in fahrenheit is {}", convert_to_fahrenheit(temperature));
+            break
+        } else if option == "2" {
+            println!("Your temp in celsius is {}", convert_to_celsius(temperature));
+            break
+        }
+    }
+}
+
+fn convert_to_fahrenheit(temp: f32) -> f32 {
+    let multiplicative: f32 = 9.0/5.0;
+    (temp * multiplicative) + 32.0
+}
+
+fn convert_to_celsius(temp: f32) -> f32 {
+    let multiplicative: f32 = 5.0/9.0;
+    (temp - 32.0) * (multiplicative)
+}
